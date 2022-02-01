3d330ebc702b0613b7d96616d4a7987be803a3cf
hamadatomoki <hamada.tomoki01@gmail.com>
2022-02-01 11:25:10 +0900

    :tada: Begin CameraSample project

---
+++ CameraSample/CameraSample/CameraSampleApp.swift
@@ -0,0 +1,18 @@
+//
+//  CameraSampleApp.swift
+//  CameraSample
+//
+//  Created by 浜田知季 on 2022/02/01.
+//
+//
+
+import SwiftUI
+
+@main
+struct CameraSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CameraSample/CameraSample/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  CameraSample
+//
+//  Created by 浜田知季 on 2022/02/01.
+//
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
